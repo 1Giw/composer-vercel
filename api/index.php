eb407f38843f91536de2a6e627deab3b15be5e76
mauludinegi <mauludinegi@gmail.com>
2024-09-06 14:54:05 +0700

    f

--- api/index.php
+++ api/index.php
@@ -4,6 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
 
-// Redirect ke index.php di root folder
-header('Location: /index.php');
-exit;
+throw new RuntimeException('Hello Tracy!');
